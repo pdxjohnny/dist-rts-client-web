3d5722690efbab559f2e5e07df786e2db619c4d0
John Andersen <johnandersenpdx@gmail.com>
2015-08-06 13:57:45 -0700

    Added javascript client

---
+++ api.js
@@ -0,0 +1,46 @@
+var make_api = function () {
+  this.connected = false;
+  this.ws = false;
+  // Set the default server
+  this.change_server(location.origin.split("//")[1]);
+  // Handlers for messages
+  return this;
+}
+
+make_api.prototype.change_server = function (url) {
+  this.origin = "ws://" + location.origin.split("//")[1] + "/ws";
+}
+
+make_api.prototype.connect = function () {
+  this.ws = new WebSocket(this.origin);
+  this.ws.onopen = this.onopen.bind(this)
+  this.ws.onclose = this.onclose.bind(this)
+  this.ws.onmessage = this.onmessage.bind(this)
+}
+
+make_api.prototype.onopen = function (data) {
+  this.connected = true;
+}
+
+make_api.prototype.onclose = function (data) {
+  this.connected = false;
+}
+
+make_api.prototype.onmessage = function (data) {
+  try {
+    data = JSON.parse(data["data"])
+  } catch (err) {
+    console.error("Could not decode", data["data"]);
+  }
+  if (typeof data["Method"] !== "undefined" &&
+    typeof this[data["Method"]] === "function") {
+    this[data["Method"]](data);
+  }
+}
+
+make_api.prototype.send = function (data) {
+	if (this.connected) {
+		data = JSON.stringify(data)
+		this.ws.send(data)
+	}
+}
